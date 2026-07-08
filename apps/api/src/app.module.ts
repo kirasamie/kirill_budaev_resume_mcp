f94121f44b9746eed3c148183c437b87a713121b
knbudaev <KNBudaev@pulse.insure>
2026-07-08 18:39:53 +0500

    Feature: fix module import

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { HealthModule } from './health';
 import { PortfolioModule } from './portfolio';
 import { ToolsModule } from './tools';
 
 @Module({
-  imports: [HealthModule, PortfolioModule, ToolsModule],
+  imports: [PortfolioModule, ToolsModule],
 })
 export class AppModule {}
